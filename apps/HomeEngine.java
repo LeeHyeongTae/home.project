6dad4f5fcc169a542ec717a6b0d724414c2d3186
LeeHyengTae <tedd911@icloud.com>
2020-04-07 00:06:54 +0900

    00:06

--- apps/HomeEngine.java
+++ apps/HomeEngine.java
@@ -6,9 +6,13 @@ public class HomeEngine {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("프로그램을 실행합니다.\n "
-				+ "0.종료 \n "
-				+ "1. 점수계산\n");
+		Member[] members = new Member[3];
+		System.out.println("0. 종료 "
+				+ "1. 점수계산"
+				+ "2. 회원가입"
+				+ "3. 회원목록"
+				+ "4. 회원 나이순 정렬");
+		
 		while (true) {
 			switch(scanner.nextInt()) {
 				case 0: 
@@ -28,9 +32,65 @@ public class HomeEngine {
 						System.out.println(String.format("[%s: 총점: %d 평균: %d 학점: %s]", 
 								school.getName(), school.total(), school.average(), school.grade()));
 						break;
+				case 2: 
+						System.out.println("회원가입");
+						for(int i=0; i<3; i++) {
+						members[i] = join(scanner);
+						}
+						break;
+				case 3: 
+						System.out.println("가입된 회원 목록");
+						for(int i=0; i<3; i++) {
+						System.out.println(String.format("[이름:%s ID:%s PW:%s 나이:%d]\n", 
+											members[i].getUserName(),
+											members[i].getUserId(),
+											members[i].getUserPw(),
+											members[i].getUserAge()));
+						}
+						break;
+				case 4:
+						System.out.println("나이순으로 정렬된 회원 목록입니다.\n");
+						if(members[0].getUserAge()>members[1].getUserAge()) {
+							if(members[1].getUserAge()>members[2].getUserAge()) {
+								System.out.println(String.format("%s \n %s \n %s \n", 
+										members[0].getUserName(), members[1].getUserName(), members[2].getUserName()));
+							}else if(members[0].getUserAge()>members[2].getUserAge()){
+								System.out.println(String.format("%s \n %s \n %s \n", 
+										members[0].getUserName(), members[2].getUserName(), members[1].getUserName()));
+							}else {
+								System.out.println(String.format("%s \n %s \n %s \n", 
+										members[2].getUserName(), members[0].getUserName(), members[1].getUserName()));
+							}
+						}else if(members[1].getUserAge()>members[2].getUserAge()) {
+							if(members[0].getUserAge()>members[2].getUserAge()) {
+								System.out.println(String.format("%s \n %s \n %s \n", 
+										members[1].getUserName(), members[0].getUserName(), members[2].getUserName()));
+							}else {
+								System.out.println(String.format("%s \n %s \n %s \n", 
+										members[1].getUserName(), members[2].getUserName(), members[0].getUserName()));
+							}
+						}else {
+							System.out.println(String.format("%s \n %s \n %s \n", 
+									members[2].getUserName(), members[1].getUserName(), members[0].getUserName()));
+						}
+						break;
 			}
 		}
 		
 	}
+	public static Member join(Scanner scanner) {
+		Member member = new Member();
+		System.out.println("이름 입력");
+		member.setUserName(scanner.next());
+		System.out.println("ID 입력");
+		member.setUserId(scanner.next());
+		System.out.println("PW 입력");
+		member.setUserPw(scanner.next());
+		System.out.println("나이 입력");
+		member.setUserAge(scanner.nextInt());
+		
+		return member;
+	}
 
 }
+
---
+++ apps/Member.java
@@ -0,0 +1,33 @@
+package com.bit.apps;
+
+public class Member {
+	private String userName;
+	private String userId;
+	private String userPw;
+	private int userAge;
+	
+	public void setUserName(String setUserName) {
+		userName = setUserName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserId(String setUserId) {
+		userId = setUserId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserPw(String setUserPw) {
+		userPw = setUserPw;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserAge(int setUserAge) {
+		userAge = setUserAge;
+	}
+	public int getUserAge() {
+		return userAge;
+	}
+}
